5335b940a6f3ffb99085344a7095fe180651bd3c
AthenaKiriakoulis <93063887+AthenaKiriakoulis@users.noreply.github.com>
2022-09-14 22:08:43 -0400

    Update fix_me.py

--- fix_me.py
+++ fix_me.py
@@ -1,2 +1,16 @@
 # 2 + 2 = ?
 # Print the answer right below this line!
+print("2")
+#Number 15, Burger King Foot Lettuce
+#The last thing you want on your Burger King burger is someone's 
+#foot fungus, but as it turns out, that might be what you get. 
+#A 4channer uploaded a photo anonymously to the site showcasing his feet in a plastic bin of lettuce, 
+#with the statement: "This is the lettuce you eat at Burger King." Admittedly, he had shoes on but, that's even worse.
+#The post went live at 11:38PM on July 16 and a mere 20 minutes later, the Burger King in question was alerted to the rogue employee. 
+#at least, I hope he's rogue. How did it happen? Well, the BK employee hadn't removed the Exif data from the uploaded photo, 
+#which suggested the culprit was somewhere in Mayfield Heights, Ohio. This was at 11:47. 3 minutes later, at 11:50, 
+#the Burger King branch address was posted, with wishes of happy unemployment. 5 minutes later, the news station was contacted
+#by another 4channer. And 3 minutes later, at 11:58, a link was posted. Bk's "Tell Us About US" online form. 
+#The foot photo otherwise known as Exhibit A, was attached. Cleveland Scene Magazine contacted the BK in question the next day. When questioned, 
+#the breakfast shift manager said, "Oh, I know who that is. He's getting fired."
+#Mystery, Solved, by 4chan, now we can all go back to eating our fast food in peace.
